a03c627482b047c01d37164569c3b8ddf1a82f4f
Gabrik65 <g.valenzuela10@ufromail.cl>
2025-04-10 09:34:40 -0400

    Comit 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        menu();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void menu(){
+        String[][] size = sizeM();
+    }
+    public static String[][] sizeM(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese No de filas de matriz");
+        int filas = getInt(sc);
+        System.out.println("Ingrese No de columnas de matriz");
+        int columnas = getInt(sc);
+        return new String[filas][columnas];
+    }
+
+    public static int getInt(Scanner sc) {
+        String numero = sc.next();
+        int num = -1;
+        try {
+            num = Integer.parseInt(numero); // Intenta convertir el String a int
+        } catch (NumberFormatException e) {
+            System.out.println("No es un número válido");
+            menu();
+        }
+        return num;
+    }
+
+    public static boolean validarDimensiones(int filas, int columnas){
+        if (filas != 0 && columnas != 0) {
+            return true;
+        }else {
+            System.out.println("Datos no validos");
+            menu();
+            return false;
         }
     }
 }
